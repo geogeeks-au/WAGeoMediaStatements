fba1cfd1087cfe7e5d03961f12a692bc5ce8a689
Geogeek <geomedia@pg02.geogeeks.org.au>
2016-06-14 11:25:13 -0400

    Created  django app

---
+++ geogeeksms/geostatements/models.py
@@ -0,0 +1,10 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+class StatementLocation(models.Model):
+	pass
+
+class GeoStatement(models.Model):
+	pass
