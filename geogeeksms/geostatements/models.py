ece429e33ba38fbf6879bf2aaa1bd5b1bbe2ab38
Cameron Poole <Cameron.j.poole@gmail.com>
2016-06-18 22:12:14 +0800

    JSON Field import

--- geogeeksms/geostatements/models.py
+++ geogeeksms/geostatements/models.py
@@ -1,15 +1,17 @@
 from __future__ import unicode_literals
+from django.contrib.postgres.fields import JSONField
+
 
 from django.db import models
 
 # Create your models here.
 class StatementLocation(models.Model):
 	location_tag = models.CharField(max_length=200)
-	geocoded_data = models.JSONField()
+	geocoded_data = JSONField()
 	geom = models.GeometryField()
 
 class GeoStatement(models.Model):
 	link = models.UrlField()
 	statement = models.TextField()
-	json = models.JSONField()
+	json = JSONField()
 	location = models.ManyToManyField(StatementLocation)
