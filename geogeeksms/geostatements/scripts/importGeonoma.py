64198580e0b6d97a5c7dcf5d71e54fab05af1481
Cameron Poole <Cameron.j.poole@gmail.com>
2016-07-07 19:42:02 +0800

    Finalised geonoma script

--- geogeeksms/geostatements/scripts/importGeonoma.py
+++ geogeeksms/geostatements/scripts/importGeonoma.py
@@ -8,46 +8,31 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "../../../geogeeksms/"))
 os.environ["DJANGO_SETTINGS_MODULE"] = "geogeeksms.settings"
 django.setup()
 
-from geostatements.models import Gazetteer
+from geostatements.models import Geonoma
 from django.contrib.gis.utils import LayerMapping
 
-z = zipfile.ZipFile('GazetteerOfAustralia2012Package.zip', 'r')
-f = z.extract('Gazetteer2012_GML.zip')
-gz = zipfile.ZipFile(f, 'r')
-gf = gz.extract('Gazetteer2012GML.gml')
-ds = DataSource(gf)
+ds = DataSource('GeographicNamesGEONOMALGATE_013/GeographicNamesGEONOMALGATE_013_1.shp')
 layer = ds[0]
 
-gazetteer_mapping = {
-    'gml_id': 'gml_id',
-    'objectid': 'OBJECTID',
-    'record_id': 'RECORD_ID',
-    'name': 'NAME',
-    'feat_code': 'FEAT_CODE',
-    'cgdn': 'CGDN',
-    'authority_id': 'AUTHORITY_ID',
-    'concise_gaz': 'CONCISE_GAZ',
-    'latitude': 'LATITUDE',
-    'lat_degrees': 'lat_degrees',
-    'lat_minutes': 'lat_minutes',
-    'lat_seconds': 'lat_seconds',
-    'longitude': 'LONGITUDE',
-    'long_degrees': 'long_degrees',
-    'long_minutes': 'long_minutes',
-    'long_seconds': 'long_seconds',
-    'state_id': 'STATE_ID',
-    'status': 'STATUS',
-    'map_100k': 'MAP_100K',
-    'place_id': 'Place_ID',
-    'variant_name': 'VARIANT_NAME',
-    'postcode': 'POSTCODE',
-    'geom': 'POINT',
+geonoma_mapping = {
+'gid' : 'gid',
+'feature_nu' : 'feature_nu',
+'geographic' : 'geographic',
+'feature_cl' : 'feature_cl',
+'name_appro' : 'name_appro',
+'easting' : 'easting',
+'northing' : 'northing',
+'zone' : 'zone',
+'decimal_la' : 'decimal_la',
+'decimal_lo' : 'decimal_lo',
+'geom' : 'POINT',
 }
+
 
 
 def run(verbose=True):
     lm = LayerMapping(
-        Gazetteer, gf, gazetteer_mapping,
+        Geonoma, gf, geonoma_mapping,
     )
     lm.save(strict=True, verbose=verbose)
 
